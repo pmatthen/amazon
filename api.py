2aeb0ff8df73a061637d0f193f366ba3d877ceda
pmatthen <poulosem@gmail.com>
2019-03-17 15:41:13 +0530

    Flask Jinja HTML OMGgit add .

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	username = 'Poulose'
-	return render_template('home.html', title = 'home', name = username)
+	return render_template('home.html', title = 'Home', name = username, home = 'home')
 
 @app.route('/about')
 def about():
-	return render_template('about.html', title = 'about')
+	return render_template('about.html', title = 'About')
 
 @app.route('/contact')
 def contact():
-	return render_template('contact.html', title = 'contact')
+	return render_template('contact.html', title = 'Contact')
 
 if __name__ == '__main__':
 	app.run(debug = True)
